55da1c08a4171376ba0b056fad3fed28834736f9
Aaron Haim <ahaim5357@gmail.com>
2024-04-25 13:48:00 +0000

    Add development environment and setup

---
+++ setup-lak24.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(lme4)
+
+source('lak24functions.R')
+
+# Coded data
+d_coded <- read_csv('lak24-coded-utterances.csv')
+
+# # Tutor logs
+d_tutor <- read_delim('ds5371_tx_All_Data_7671.txt', delim='\t') %>%
+  janitor::clean_names()
+
+# Originally sampled utterances
+df_utterance = read.csv("transcripts-with-logdata-reference-lak24.csv")
+
+d_analysis <- aggregate_data(d_coded, d_tutor, df_utterance) %>%
+  add_loop_variables()
+
+write_csv(d_analysis, 'd_analysis.csv') # for RQ3 analysis in Python
---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+error=0
+
+if find . -regex '\./ds[0-9]+_tx_[A-Za-z_]+_[0-9]+\.txt'; then
+    # The file has already been processed
+    echo 'Transaction file already has its timestamp stripped, skipping...'
+elif find . -regex '\./ds[0-9]+_tx_[A-Za-z_]+_[0-9]+_[0-9_]+\.txt'; then
+    # The file hasn't been processed yet
+
+    ## Rename the transactional file to parse out the timestamp
+    rename -n 's/(ds\d+_tx_[A-Za-z_]+_\d+)_[0-9_]+\.txt/${1}\.txt/' *.txt
+    echo 'Renamed transactional file!'
+else
+    echo 'Could not find transactional file.'
+    error=1
+fi
+
+if [[ -f './d_analysis.csv' ]]; then
+    # The setup script in R has already been ran
+    echo 'Setup script already run, skipping...'
+elif [[ -n `find . -regex '\./ds[0-9]+_tx_[A-Za-z_]+_[0-9]+\.txt'` && -f './lak24-coded-utterances.csv' && -f './transcripts-with-logdata-reference-lak24.csv' ]]; then
+    # The setup script in R has not been ran yet
+
+    ## Construct the dataset to run the other R scripts
+    Rscript ./setup-lak24.R
+    echo 'Constructed missing datasets!'
+else
+    echo 'Missing one or more dataset files.'
+    error=1
+fi
+
+if [ $error -ne 0 ]; then
+    # Report error
+    echo "Please follow the steps under 'Setup' in the README to get started. Then run this script again."
+else
+    echo "Setup successfully finished!"
+fi
